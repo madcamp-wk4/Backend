4fde6d38e69cb36a3c79374c02d101ff066c82ec
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-01-18 23:04:36 +0900

    db 연결 성공

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { testConnection } = require('./DB/db'); // testConnection 가져오기
+require('dotenv').config();
+
 const app = express();
 
+// Express 라우터
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
+
+// 데이터베이스 연결 테스트
+testConnection();
 
 const PORT = 3000;
 app.listen(PORT, () => {

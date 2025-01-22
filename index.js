49e6209fa5f595d06e11f8bead4bdfc33c73554c
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-01-23 05:45:26 +0900

    feat : AI 응답 기억해야할점, 특징 넣기 성공 / 사랑의 5 요소 추출 완료 but DB insert는 아직.. 애인 정보 가져오는 api 작업중

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dateRecordsRoutes = require("./routes/dateRecords");
 const messageRoutes = require("./routes/message");
 const messageAnalysisRoutes = require("./routes/messageAnalysis");
 const setupSwagger = require("./swagger");
+const login = require('./routes/login')
+const signup = require('./routes/signup')
+const loverProfile = require('./routes/loverProfile')
 require("dotenv").config(); // 환경변수 중요
 const cors = require("cors");
 const app = express();
@@ -16,6 +19,9 @@ app.use(express.json());
 app.use("/date-records", dateRecordsRoutes);
 app.use("/messages", messageRoutes);
 app.use("/message-analysis", messageAnalysisRoutes);
+app.use("/login", login)
+app.use("/signup", signup)
+app.use("/profile/lover", loverProfile)
 
 // Root route
 app.get("/", (req, res) => {

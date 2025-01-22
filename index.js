dd27ffa32fb7b1ddef19471668277b62b455a763
Sanggeun Oh <osg2313@kaist.ac.kr>
2025-01-22 21:57:03 +0900

    BACK H CLEAR

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const dateRecordsRoutes = require("./routes/dateRecords");
 const messageRoutes = require("./routes/message");
 const messageAnalysisRoutes = require("./routes/messageAnalysis");
+const scoreRecordRoutes = require("./routes/scoreRecord"); // "채점" 라우트로 변경
+
 const setupSwagger = require("./swagger");
 require("dotenv").config(); // 환경변수 중요
 const cors = require("cors");
@@ -12,10 +14,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
+
 // Routes
 app.use("/date-records", dateRecordsRoutes);
 app.use("/messages", messageRoutes);
 app.use("/message-analysis", messageAnalysisRoutes);
+app.use("/score-record", scoreRecordRoutes); // 경로 추가
 
 // Root route
 app.get("/", (req, res) => {
---
+++ prisma/migrations/20250122123540_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `DateAnalysisResult` MODIFY `reason` TEXT NOT NULL;
